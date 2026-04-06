4591534e9bd1a34789711e4f24e889d441e0fd15
Jeroen Akkerman <Jeroen_akkerman1@hotmail.com>
2026-04-07 00:23:03 +0200

    Remove unused API route for greeting by name in main application file, streamlining the codebase.

--- src/main.ts
+++ src/main.ts
@@ -5,13 +5,5 @@ export const app = new App<State>();
 
 app.use(staticFiles());
 
-// this is the same as the /api/:name route defined via a file. feel free to delete this!
-app.get("/api2/:name", (ctx) => {
-  const name = ctx.params.name;
-  return new Response(
-    `Hello, ${name.charAt(0).toUpperCase() + name.slice(1)}!`,
-  );
-});
-
 // Include file-system based routes here
 app.fsRoutes();
