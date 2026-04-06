0f6bdbe0a8fb3c2cc51459eee44c608a9eb0430e
Jeroen Akkerman <Jeroen_akkerman1@hotmail.com>
2026-04-07 00:23:46 +0200

    Remove helloMiddleware from the middleware stack and delete its implementation, streamlining the codebase.

--- src/middlewares/hello.ts
+++
@@ -1,6 +0,0 @@
-import { define } from "../utils.ts";
-
-export const helloMiddleware = define.middleware(async (ctx) => {
-  ctx.state.shared = "hello";
-  return await ctx.next();
-});
--- src/routes/_middleware.ts
+++ src/routes/_middleware.ts
@@ -1,5 +1,4 @@
-import { helloMiddleware } from "../middlewares/hello.ts";
 import { loggerMiddleware } from "../middlewares/logger.ts";
 import { sessionMiddleware } from "../middlewares/session.ts";
 
-export default [sessionMiddleware, helloMiddleware, loggerMiddleware];
+export default [sessionMiddleware, loggerMiddleware];
